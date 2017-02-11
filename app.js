e9f26c5486511996836c3e22e5ebd8db0bf6d229
Sam <sbgranderson@gmail.com>
2017-02-11 20:29:32 +0000

    port change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static('public'))
 //  res.send('This is the website of Samuel Benjamin!')
 //})
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
 })
 
